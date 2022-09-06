0642333c16b8093acb3b09925d823b09ce68d4a3
PatrickBDJ <91883891+PatrickBDJ@users.noreply.github.com>
2022-09-06 10:49:20 +0200

    slettet kommentar

--- script.js
+++ script.js
@@ -4,7 +4,5 @@ myTextArea.addEventListener('input', () =>{
     myTextArea.value.length;
     console.log(myTextArea.value.length);
     // console.log('got input!');
-    document.getElementById("counter-modal").innerHTML = (myTextArea.value.length + " / " + "255 karakterer tilbage");
-    // hive fat i div med counter-modal
-    // input myTextArea.value.length ved 0s plads
+    document.getElementById("counter-modal").innerHTML = (myTextArea.value.length + " / " + "255 karakterer tilbage");  
 });
