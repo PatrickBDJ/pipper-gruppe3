635f628f3c2c60e553b42b2243d9f0057a32e94f
PatrickBDJ <91883891+PatrickBDJ@users.noreply.github.com>
2022-09-06 09:42:18 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const myTextArea = document.getElementById('pip-content-modal');
+const characterCounter = document.getElementById('counter-modal');
+
+myTextArea.addEventListener('input', () =>{
+    console.log('got input!');
+});
