1da2c15621d5d0ab167128a0c925aa090c667310
PatrickBDJ <91883891+PatrickBDJ@users.noreply.github.com>
2022-09-07 13:50:43 +0200

    % kommentar

--- script.js
+++ script.js
@@ -2,7 +2,5 @@ const myTextArea = document.getElementById('pip-content-modal');
 
 myTextArea.addEventListener('input', () =>{
     myTextArea.value.length;
-    console.log(myTextArea.value.length);
-    // console.log('got input!');
     document.getElementById("counter-modal").innerHTML = (myTextArea.value.length + " / " + "255 karakterer tilbage");  
 });
