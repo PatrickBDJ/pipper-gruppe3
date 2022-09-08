74bc3eb494669d073a88724979174a2ac7b538a3
PatrickBDJ <dyva@live.dk>
2022-09-08 10:31:42 +0200

    Textarea1

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ myTextArea.addEventListener('input', () =>{
     myTextArea.value.length;
     document.getElementById("counter-modal").innerHTML = (myTextArea.value.length + " / " + "255 karakterer tilbage");  
 });
+const myTextArea1 = document.getElementById('pip-content');
+
+myTextArea1.addEventListener('input', () =>{
+    myTextArea.value.length;
+    document.getElementById("counter-content").innerHTML = (myTextArea1.value.length + " / " + "255 karakterer tilbage");  
+});
